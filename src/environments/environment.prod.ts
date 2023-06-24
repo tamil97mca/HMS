4cc80bb9bf4b89b56d057459f3f205e62697b5a6
tami2847 <tamilselvan.kuppuraj@chainsys.com>
2023-06-24 10:34:54 +0530

    Environment setup added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBBlRiLK4jNWns2aqrOu-fcAWgOcXd2xYs",
+    authDomain: "hospitalmanagementsystemhms.firebaseapp.com",
+    projectId: "hospitalmanagementsystemhms",
+    storageBucket: "hospitalmanagementsystemhms.appspot.com",
+    messagingSenderId: "157642067981",
+    appId: "1:157642067981:web:171d11edc9c8b36b2a8544"
+  }
 };
